383f9678959a5d94bdacab5bcc689e16cdf74aa3
oumou13 <245577736+amadoudam568-cpu@users.noreply.github.com>
2026-08-11 01:46:31 +0000

    Update sync.js

--- api/sync.js
+++ api/sync.js
@@ -1,11 +1,3 @@
-// Note : Sur Vercel (gratuit), les données s'effacent après quelques minutes d'inactivité.
-// C'est parfait pour un test, mais pour une boutique réelle, il faudra lier une base de données.
-let cloudStore = {}; 
-
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    cloudStore = req.body;
-    return res.status(200).json({ status: "Success" });
-  }
-  res.status(200).json(cloudStore);
-}
+module.exports = (req, res) => {
+  res.status(200).json({ status: "Vercel Pret" });
+};
